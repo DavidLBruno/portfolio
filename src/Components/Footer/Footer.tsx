707a14a47e1da66188ddc741c2076aba5a21856f
Bruno David <93402677+DavidLBruno@users.noreply.github.com>
2023-04-16 23:04:18 -0300

    firts commit

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">HOLA</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>wenotas</div>;
+};
+
+export default Home;
