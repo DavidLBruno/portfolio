e230c1c31b4a88efa5afd3c8436906e7529d53e8
Bruno David <93402677+DavidLBruno@users.noreply.github.com>
2023-08-21 17:48:14 -0300

    navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent {
+  hamburguer: boolean = false;
+  items = [
+    {
+      title: "Home",
+      link: "",
+      icon: "",
+    },
+    {
+      title: "Proyectos",
+      link: "projects",
+      icon: "",
+    },
+    {
+      title: "Sobre mi",
+      link: "about",
+      icon: "",
+    },
+    {
+      title: "Tecnologias",
+      link: "tecnologies",
+      icon: "",
+    },
+  ];
 
+  handleMenu() {
+    this.hamburguer = !this.hamburguer;
+  }
 }
